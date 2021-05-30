e61ea44da7551c7c8e1d714d3da817e73b428e68
Albrecht Uhlig <albrecht.uhlig@gmail.com>
2021-05-30 04:08:54 +0200

    mousewheelspeed adjustment, no delay, quicker

--- keyboards/handwired/mercicorn/dactyl_manu_22_3/config.h
+++ keyboards/handwired/mercicorn/dactyl_manu_22_3/config.h
@@ -30,7 +30,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 10
 
+// meine einstellungen
 #define TAP_CODE_DELAY 10
+//#define MOUSEKEY_DELAY            	10	
+//#define MOUSEKEY_INTERVAL          	10
+//#define MOUSEKEY_MAX_SPEED         	3
+//#define MOUSEKEY_TIME_TO_MAX       	20
+#define MOUSEKEY_WHEEL_DELAY        0
+#define MOUSEKEY_WHEEL_INTERVAL     50
+#define MOUSEKEY_WHEEL_MAX_SPEED   	2
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 	20
 
 /*
  * Keyboard Matrix Assignments
